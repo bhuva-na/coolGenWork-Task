fa7c421259aba15fdb82a7e8adc41ceab48f32dd
bhuva-na <rizanabhuvana@gmail.com>
2024-08-21 13:59:04 +0530

    aft

--- T1--13082024/frontend/coe/src/components/interships.js
+++ T1--13082024/frontend/coe/src/components/interships.js
@@ -8,7 +8,7 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import colleagues from './colleagues.jpg';
 
-const Internships = () => {
+const Interships = () => {
   const [hoveredCard, setHoveredCard] = useState(null);
 
   const handleMouseEnter = (index) => {
@@ -142,4 +142,4 @@ const Internships = () => {
   );
 };
 
-export default Internships;
+export default Interships;
